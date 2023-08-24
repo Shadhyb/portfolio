ed76bd4dd629e476368a2560266891b781b8bc44
Shadhyb <pietro.rimoli89@gmail.com>
2023-08-24 14:42:28 +0200

    fix:useEffect

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -18,7 +18,7 @@ export const Banner = () => {
     return () => {
       clearInterval(ticker);
     };
-  }, [delta, text]);
+  });
 
   const tick = () => {
     let i = loopNum % toRotate.length;
